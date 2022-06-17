61ff577b7cb8708e0f1e070813b161da060b0d82
Pedro Miranda <20952412+pedrodotmc@users.noreply.github.com>
2022-06-16 22:20:02 -0500

    PR pipeline (#21)
    * PR pipeline
    
    * Fix YAMLLINT issues
    
    * Run pulumi preview
    
    * Fix yq command
    
    * Remove yq flag
    
    * Fix Pulumi path
    
    * Fix AWS Role
    
    * Set StringLike in role policy
    
    * Set audience in OIDC provider
    
    * Trigger CI
    
    * Add GitHub thumbprint
    
    * Trigger CI
    
    * Trigger CI - Create production stack
    
    * Trigger CI - Set PULUMI_CONFIG_PASSPHRASE
    
    * Set PULUMI_CONFIG_PASSPHRASE in workflow
    
    * Setup node
    
    * Run pulumi preview subsequently
    
    * Unlock pulumi state
    
    * Add working directory to yarn install
    
    * Add write permissions to PR
    
    * Set fixed role name
    
    * Add main and destroy pipelines

--- aws/lib/0-bootstrap/pulumi-cicd.ts
+++ aws/lib/0-bootstrap/pulumi-cicd.ts
@@ -9,25 +9,44 @@ export class PulumiCICD {
 
 
     constructor (private readonly config: pulumi.Config) {
-        this.setAwsRolePolicy(this.setAwsRole(this.setGitHubIdentityProvider()))
         const secretProviderKeyId = this.setSecretProviderKey(`${this.config.get('organizationName')}/${pulumi.getStack()}/pulumi-secret-provider`).keyId
         const backendBucketName = this.setBackendBucket(`${this.config.get('organizationName')}-${pulumi.getStack()}-pulumi-backend`).bucket
+        this.setAwsRolePolicy(this.setAwsRole(this.setGitHubIdentityProvider()))
 
         this.secretProviderKeyId = secretProviderKeyId
         this.backendBucketName = backendBucketName
     }
 
 
+    private setSecretProviderKey = (keyName: string): aws.kms.Key =>
+        new aws.kms.Key(keyName, {
+            description: 'Key for Pulumi secret provider',
+            tags: {
+                Name: keyName
+            }
+        })
+    
+    
+    private setBackendBucket = (bucketPrefix: string): aws.s3.Bucket =>
+        new aws.s3.Bucket(bucketPrefix, {
+            bucketPrefix,
+            tags: {
+                Name: bucketPrefix
+            }
+        })
+
+
     private setGitHubIdentityProvider = (): aws.iam.OpenIdConnectProvider =>
         new aws.iam.OpenIdConnectProvider('github-oidc', {
             url: 'https://token.actions.githubusercontent.com',
-            clientIdLists: [`https://${this.config.get('githubOrg')}`],
-            thumbprintLists: []
+            clientIdLists: ['sts.amazonaws.com'],
+            thumbprintLists: ['6938fd4d98bab03faadb97b34396831e3780aea1']
         })
 
-    
+
     private setAwsRole = (gitHubIdentityProvider: aws.iam.OpenIdConnectProvider): aws.iam.Role =>
         new aws.iam.Role('pulumi-cicd', {
+            name: `${this.config.get('organizationName')}-pulumi-cicd`,
             assumeRolePolicy: aws.iam.getPolicyDocumentOutput({
                 statements: [
                     {
@@ -37,17 +56,24 @@ export class PulumiCICD {
                             identifiers: [gitHubIdentityProvider.arn]
                         }],
                         actions: ['sts:AssumeRoleWithWebIdentity'],
-                        conditions: [{
-                            test: 'StringLike',
-                            variable: 'token.actions.githubusercontent.com:sub',
-                            values: [`repo:${this.config.get('githubOrg')}`]
-                        }]
+                        conditions: [
+                            {
+                                test: 'StringLike',
+                                variable: 'token.actions.githubusercontent.com:aud',
+                                values: ['sts.amazonaws.com']
+                            },
+                            {
+                                test: 'StringLike',
+                                variable: 'token.actions.githubusercontent.com:sub',
+                                values: [`repo:${this.config.get('githubOrg')}/${this.config.get('githubRepo')}:*`]
+                            }
+                        ]
                     }
                 ],
             }).json
         })
-    
-    
+
+
     private setAwsRolePolicy = (role: aws.iam.Role): aws.iam.RolePolicy =>
         new aws.iam.RolePolicy('pulumi-cicd', {
             role,
@@ -71,23 +97,5 @@ export class PulumiCICD {
                     }
                 ],
             }).json
-        })
-
-
-    private setSecretProviderKey = (keyName: string): aws.kms.Key =>
-        new aws.kms.Key(keyName, {
-            description: 'Key for Pulumi secret provider',
-            tags: {
-                Name: keyName
-            }
-        })
-    
-    
-    private setBackendBucket = (bucketPrefix: string): aws.s3.Bucket =>
-        new aws.s3.Bucket(bucketPrefix, {
-            bucketPrefix,
-            tags: {
-                Name: bucketPrefix
-            }
         })
 }
